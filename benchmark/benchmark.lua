0c174084231b56216e8e9deedeca83d830b74ae0
Famiu Haque <famiuhaque@protonmail.com>
2021-09-05 12:20:43 +0600

    refactor(benchmark): convert benchmark code to lua

---
+++ benchmark/benchmark.lua
@@ -0,0 +1,10 @@
+-- Feline benchmark test
+-- Requires 'norcalli/profiler.nvim' as an opt plugin
+-- Make sure you have feline.nvim and its dependencies installed as start plugins
+-- Run from Feline top-level directory using:
+-- env AK_PROFILER=1 nvim --noplugin -u benchmark/benchmark.lua > /dev/null 2>&1 | less
+
+vim.cmd('packadd profiler.nvim')
+
+local profiler = require('profiler')
+profiler.wrap(require('feline').setup())
