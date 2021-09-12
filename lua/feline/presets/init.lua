a703bed33b30f41fe73b50c85b5434f9ee6762f1
Famiu Haque <famiuhaque@protonmail.com>
2021-09-12 19:19:55 +0600

    perf(presets): only load the preset being used

--- lua/feline/presets/init.lua
+++ lua/feline/presets/init.lua
@@ -1,7 +1,15 @@
-local M = {}
+-- Use an immutable table to only automatically load the preset that's are being used
+local M = setmetatable({}, {
+    __index = function(_, key)
+        local ok, result = pcall(require, 'feline.presets.' .. key)
+        if ok then return result else return nil end
+    end,
 
-M.default = require('feline.presets.default')
-M.noicon = require('feline.presets.noicon')
+    __newindex = function(_, _, _)
+    end,
+
+    __metatable = false
+})
 
 return M
 
