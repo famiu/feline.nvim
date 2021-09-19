079c6140e0e7940c01057df3b9b78b87023473eb
Famiu Haque <famiuhaque@protonmail.com>
2021-09-19 22:40:49 +0600

    fix(benchmark/startup): add gitsigns setup

--- benchmark/startup.lua
+++ benchmark/startup.lua
@@ -1,7 +1,7 @@
 -- Feline startup benchmark test
 -- Make sure you have feline.nvim and its dependencies installed and
 -- run from Feline top-level directory using:
--- env AK_PROFILER=1 nvim -u benchmark/startup.lua > /dev/null 2>&1 | less
+-- env AK_PROFILER=1 nvim --noplugin -u benchmark/startup.lua > /dev/null 2>&1 | less
 
 local opt = vim.opt
 local fn = vim.fn
@@ -16,6 +16,10 @@ if not pcall(require, 'profiler') then
         fn.system({'git', 'clone', 'https://github.com/norcalli/profiler.nvim', install_path})
     end
 end
+
+-- Setup gitsigns
+local ok, gitsigns = pcall(require, 'gitsigns')
+if ok then gitsigns.setup() end
 
 -- Start benchmark
 require('profiler').wrap(require('feline').setup())
