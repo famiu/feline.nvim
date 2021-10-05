00eecff5ca419bd72854b7f877fe2452b9e8f3f4
Famiu Haque <famiuhaque@protonmail.com>
2021-10-05 11:22:18 +0600

    refactor: reorganize statusline ffi code

--- lua/feline/statusline_ffi.lua
+++ lua/feline/statusline_ffi.lua
@@ -30,13 +30,12 @@ local char_u_buf_t = ffi.typeof('char_u[?]')
 local char_u_str_t = ffi.typeof('char_u*')
 
 -- Statusline string buffer
-local stlbuf_len = 256
+local stlbuf_len = 1024
 local stlbuf = char_u_buf_t(stlbuf_len)
 
--- Expand statusline expression, returns a Lua string containing plaintext with only the characters
--- that'll be displayed in the statusline
-function M.expand_statusline_expr(expr)
-    ffi.C.build_stl_str_hl(
+-- Get display width of statusline expression
+function M.get_statusline_expr_width(expr)
+    return ffi.C.build_stl_str_hl(
         ffi.C.curwin,
         stlbuf,
         stlbuf_len,
@@ -47,23 +46,6 @@ function M.expand_statusline_expr(expr)
         nil,
         nil
     )
-
-    return ffi.string(stlbuf)
-end
-
--- Get display width of statusline expression
-function M.get_statusline_expr_width(expr)
-    return tonumber(ffi.C.build_stl_str_hl(
-        ffi.C.curwin,
-        stlbuf,
-        stlbuf_len,
-        ffi.cast(char_u_str_t, expr),
-        0,
-        0,
-        0,
-        nil,
-        nil
-    ))
 end
 
 return M
