6be9ca2d26b704d4df91a3199cd0c2ab870ac9d8
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-12-02 20:51:00 +0000

    refactor!: use vim.diagnostic for lsp provider
    BREAKING CHANGE: The `diagnostics_exist` utility function now takes an integer containing the diagnostic severity instead of a string. For more info, do `:help vim.diagnostic.severity` in Neovim.

--- lua/feline/providers/lsp.lua
+++ lua/feline/providers/lsp.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 local lsp = vim.lsp
+local diagnostic = vim.diagnostic
+
+-- Initialize a local table with severity names to prevent having to create a table in every call of
+-- the diagnostic function to improve performance
+local severity_names = { "Information", "Hint", "Warning", "Error" }
 
 function M.is_lsp_attached()
     return next(lsp.buf_get_clients(0)) ~= nil
 end
 
 function M.get_diagnostics_count(severity)
-    local active_clients = lsp.buf_get_clients(0)
-
-    if not active_clients then return 0 end
-
-    local count = 0
-
-    for _, client in pairs(active_clients) do
-        count = count + lsp.diagnostic.get_count(0, severity, client.id)
+    if vim.fn.has("nvim-0.6") == 1 then
+        return vim.tbl_count(diagnostic.get(0, severity and { severity = severity }))
+    else
+        -- TODO: drop this when 0.5 is no longer used
+        return lsp.diagnostic.get_count(0, severity and severity_names[severity])
     end
-
-    return count
 end
 
 function M.diagnostics_exist(severity)
@@ -42,19 +42,19 @@ local function diagnostics(severity)
 end
 
 function M.diagnostic_errors()
-    return diagnostics('Error'), '  '
+    return diagnostics(diagnostic.severity.ERROR), '  '
 end
 
 function M.diagnostic_warnings()
-    return diagnostics('Warning'), '  '
-end
-
-function M.diagnostic_hints()
-    return diagnostics('Hint'), '  '
+    return diagnostics(diagnostic.severity.WARN), '  '
 end
 
 function M.diagnostic_info()
-    return diagnostics('Information'), '  '
+    return diagnostics(diagnostic.severity.INFO), '  '
+end
+
+function M.diagnostic_hints()
+    return diagnostics(diagnostic.severity.HINT), '  '
 end
 
 return M
