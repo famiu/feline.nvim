4218b60376e6944fee0316bfe28b32be9aa9c311
Famiu Haque <famiuhaque@protonmail.com>
2022-05-31 17:10:39 +0600

    fix: make `position` provider work correctly with tabs (#277)

--- lua/feline/providers/cursor.lua
+++ lua/feline/providers/cursor.lua
@@ -7,8 +7,15 @@ local scroll_bar_blocks = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '�
 function M.position(_, opts)
     local row, col = unpack(api.nvim_win_get_cursor(0))
 
+    local line = api.nvim_get_current_line()
+    -- Get the text before the cursor in the current line
+    local before_cursor = line:sub(1, col)
+
+    -- Replace tabs with the equivalent amount of spaces according to the value of 'tabstop'
+    before_cursor = before_cursor:gsub('\t', string.rep(' ', vim.bo.tabstop))
+
     -- Turn col from byteindex to column number and make it start from 1
-    col = vim.str_utfindex(api.nvim_get_current_line(), col) + 1
+    col = vim.str_utfindex(before_cursor) + 1
 
     if opts.padding then
         return string.format('%3d:%-2d', row, col)
